3d586539dce395f73846dfe8f2073148cd92639d
Usman Akhtar <akhtar_usman@outlook.com>
2021-05-04 21:54:57 -0400

    initialized spring boot project

---
+++ src/test/java/com/fullstack/FoodBase/FoodBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.FoodBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
