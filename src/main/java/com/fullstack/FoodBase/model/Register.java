0a65918ac8a9e9649e0b6c289f1697601233f4e3
Usman Akhtar <akhtar_usman@outlook.com>
2021-06-06 18:36:51 -0400

    Created User table

--- src/main/java/com/fullstack/FoodBase/model/Register.java
+++ src/main/java/com/fullstack/FoodBase/model/Register.java
@@ -6,6 +6,9 @@ import com.fullstack.FoodBase.validation.ValidEmail;
 import com.fullstack.FoodBase.validation.ValidUsername;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -13,22 +16,26 @@ import javax.validation.constraints.Size;
 @Data
 @PasswordMatches
 @EmailMatches
+@Entity
 public class Register {
 
     @ValidUsername
     @NotNull
     @NotBlank
+    @Id
     private String username;
 
     @ValidEmail
     @NotNull
     @NotBlank
     private String email;
+    @Transient
     private String emailConfirm;
 
     @NotNull
     @NotBlank
     @Size(min = 8)
     private String password;
+    @Transient
     private String passwordConfirm;
 }
