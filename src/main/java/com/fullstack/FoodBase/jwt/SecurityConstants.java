729f6b0d2572444dc20161e84740303e8c45a351
Usman Akhtar <akhtar_usman@outlook.com>
2021-06-08 18:26:35 -0400

    change jwt expiration to 5 mins

--- src/main/java/com/fullstack/FoodBase/jwt/SecurityConstants.java
+++ src/main/java/com/fullstack/FoodBase/jwt/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.fullstack.FoodBase.jwt;
 
 public class SecurityConstants {
 
-    public static final long EXPIRATION_TIME = 900_000; // 15 mins
+    public static final long EXPIRATION_TIME = 300_000; // 15 mins
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/database/registration";
